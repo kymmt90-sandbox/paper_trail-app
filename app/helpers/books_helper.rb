93d2fce3c2c832c034185866ecefc919e143848a
Kōhei Yamamoto <kymmt90@gmail.com>
2017-08-27 18:45:34 +0900

    List the changelog of books in books#show

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,16 @@
 module BooksHelper
+  def passivated_event_name(version)
+    case version.event
+    when 'create'
+      'created'
+    when 'update'
+      'updated'
+    when 'destroy'
+      'destroyed'
+    end
+  end
+
+  def created_at(version)
+    version.created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
 end
